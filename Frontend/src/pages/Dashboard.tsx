4ee538e2cc585e2b491d3d11b78b1260a91219a8
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-12-12 20:12:34 +0530

    Backend linked to frontend for custom and normal url shortening
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,38 @@
 import "./Dashboard.scss";
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 import MiniRoute from "../assets/MiniRoute.png";
 
 function Dashboard() {
   const [miniRoute, setMiniRoute] = useState("");
+  const [longurl, setLongUrl] = useState("");
+  const [shortCode, setShortCode] = useState("");
+
+  const shortenUrl = async () => {
+    try {
+      const token = sessionStorage.getItem("jwt");
+      const res = await axios.post(
+        "http://localhost:8080/url/shorten",
+        {
+          longurl: longurl,
+          shorturl: shortCode,
+        },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      setMiniRoute("http://localhost:8080/url/redirect/" + res.data.shortUrl);
+    } catch (error: any) {
+      if (error.response) {
+        toast.error(error.response.data.Message, {
+          position: "bottom-right",
+        });
+      }
+    }
+  };
 
   return (
     <>
@@ -22,19 +51,30 @@ function Dashboard() {
             <div className="inputTitle">
               🔗 Shorten a long URL<b>*</b>
             </div>
-            <input type="text" placeholder="Enter long link here" />
+            <input
+              type="text"
+              placeholder="Enter long link here"
+              value={longurl}
+              onChange={(e) => setLongUrl(e.target.value)}
+            />
             <div className="inputTitle">✨ Customize your link</div>
-            <input type="text" placeholder="Enter custom short code" />
-            <button>Shorten URL</button>
+            <input
+              type="text"
+              placeholder="Enter custom short code"
+              value={shortCode}
+              onChange={(e) => setShortCode(e.target.value)}
+            />
+            <button disabled={!longurl} onClick={() => shortenUrl()}>
+              Shorten URL
+            </button>
             <div className="logo2">
               <img src={MiniRoute} alt="" />
             </div>
             <input
-              type="text"
-              value={miniRoute}
-              contentEditable={false}
               placeholder="Your MiniRoute"
               className="miniroute"
+              value={miniRoute}
+              readOnly
             />
           </div>
         </div>
