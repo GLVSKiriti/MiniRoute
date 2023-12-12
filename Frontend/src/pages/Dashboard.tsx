453e34e42778fe0f4a6108917220b843db34730e
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-12-12 19:16:32 +0530

    URL shortner wrapper ui in dashboard is done
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,31 +1,46 @@
 import "./Dashboard.scss";
-import { useEffect } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
+import { useState } from "react";
+import MiniRoute from "../assets/MiniRoute.png";
 
 function Dashboard() {
-  const fetchmyapi = async () => {
-    try {
-      const token = sessionStorage.getItem("jwt");
-      const res = await axios.get("http://localhost:8080/url/shorten", {
-        headers: {
-          Authorization: token,
-        },
-      });
-    } catch (error: any) {
-      if (error.response) {
-        toast.error(error.response.data.Message, {
-          position: "bottom-right",
-        });
-      }
-    }
-  };
+  const [miniRoute, setMiniRoute] = useState("");
 
-  useEffect(() => {
-    fetchmyapi();
-  }, []);
-
-  return <div>This is dashboard</div>;
+  return (
+    <>
+      <div className="dashboardWrapper">
+        <div className="navBar">
+          <div className="logo">
+            <img src={MiniRoute} alt="" />
+          </div>
+          <div className="menu">
+            <div className="myurls">My Urls</div>
+            <div className="logout">LogOut</div>
+          </div>
+        </div>
+        <div className="dashboard">
+          <div className="urlShortnerPad">
+            <div className="inputTitle">
+              🔗 Shorten a long URL<b>*</b>
+            </div>
+            <input type="text" placeholder="Enter long link here" />
+            <div className="inputTitle">✨ Customize your link</div>
+            <input type="text" placeholder="Enter custom short code" />
+            <button>Shorten URL</button>
+            <div className="logo2">
+              <img src={MiniRoute} alt="" />
+            </div>
+            <input
+              type="text"
+              value={miniRoute}
+              contentEditable={false}
+              placeholder="Your MiniRoute"
+              className="miniroute"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
